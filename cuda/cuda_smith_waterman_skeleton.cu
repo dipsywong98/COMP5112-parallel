08ae637d2cf045d0d91b47756b4a78b4cbabd0e4
dipsywong98 <ycwongal@connect.ust.hk>
2020-05-03 17:32:05 +0800

    code tidy and submit

--- cuda/cuda_smith_waterman_skeleton.cu
+++ cuda/cuda_smith_waterman_skeleton.cu
@@ -12,11 +12,6 @@ using namespace std;
  */
 
 #define max(a, b) (a > b ? a : b)
-#define min(a, b) (a < b ? a : b)
-
-inline __device__ int idx(int i, int j, int I) {
-  return j * I + i;
-}
 
 __global__ void cuda_sw(char *a, char *b, int a_len, int b_len, int *d_scores, int *max_scores, int y, int* d_scores1, int* d_scores2) {
   int tid = blockDim.x * blockIdx.x + threadIdx.x;
@@ -40,19 +35,6 @@ __global__ void cuda_sw(char *a, char *b, int a_len, int b_len, int *d_scores, i
   max_scores[tid] = max(max_scores[tid], max_score);
 }
 
-__global__ void maxOf(int *max_scores) {
-  int tid = blockDim.x * blockIdx.x + threadIdx.x;
-  int tn = blockDim.x * gridDim.x;
-  int l = tn;
-  while (l > 1) {
-    __syncthreads();
-    l = (l + 1) / 2;
-    if (tid < l && tid + l < tn) {
-      max_scores[tid] = max(max_scores[tid], max_scores[tid + l]);
-    }
-  }
-}
-
 int smith_waterman(int blocks_per_grid, int threads_per_block, char *_a, char *_b, int _a_len, int _b_len) {
   dim3 blocks(blocks_per_grid);
   dim3 threads(threads_per_block);
@@ -61,7 +43,6 @@ int smith_waterman(int blocks_per_grid, int threads_per_block, char *_a, char *_
   char *b;
   int a_len;
   int b_len;
-  int len;
   int *d_scores, *d_scores1, *d_scores2;
   int max_score = 0;
   int tn = blocks_per_grid * threads_per_block;
@@ -80,15 +61,14 @@ int smith_waterman(int blocks_per_grid, int threads_per_block, char *_a, char *_
   cudaMalloc(&d_scores2, sizeof(int) * (b_len + 1));
   cudaMalloc(&d_max_scores, sizeof(int) * tn);
   cudaMalloc(&d_a, sizeof(int) * a_len);
-  cudaMemcpy(d_a, a, sizeof(int) * a_len, cudaMemcpyHostToDevice);
   cudaMalloc(&d_b, sizeof(int) * b_len);
+  cudaMemcpy(d_a, a, sizeof(int) * a_len, cudaMemcpyHostToDevice);
   cudaMemcpy(d_b, b, sizeof(int) * b_len, cudaMemcpyHostToDevice);
   cudaMemset(d_scores, 0, sizeof(int) * (b_len+1));
   cudaMemset(d_scores1, 0, sizeof(int) * (b_len+1));
   cudaMemset(d_scores2, 0, sizeof(int) * (b_len+1));
   cudaMemset(d_max_scores, 0, sizeof(int) * tn);
 
-//  cuda_sw<<<blocks, threads>>>(a, b, a_len, b_len, d_scores, len);
   for (int y = 0; y < a_len + b_len + 1; y++) {
     cuda_sw <<< blocks, threads >>>(d_a, d_b, a_len, b_len, d_scores, d_max_scores, y, d_scores1, d_scores2);
     int* t = d_scores2;
@@ -96,25 +76,13 @@ int smith_waterman(int blocks_per_grid, int threads_per_block, char *_a, char *_
     d_scores1 = d_scores;
     d_scores = t;
   }
-  maxOf<<< blocks, threads >>>(d_max_scores);
 
   int *h_max_scores = new int[tn];
-//  int *scores = new int[len];
   cudaMemcpy(h_max_scores, d_max_scores, sizeof(int) * tn, cudaMemcpyDeviceToHost);
 
-//  cudaMemcpy(scores, d_scores, sizeof(int) * len, cudaMemcpyDeviceToHost);
-//  for (int id = 0; id < len; id++) {
-//    if (id % (b_len + 1) == 0) std::cout << std::endl;
-//    std::cout << scores[id] << " ";
-//  }
-//  std::cout << endl;
-
   for (int i = 0; i < tn; i++) {
-//    std::cout << h_max_scores[i] << " ";
     max_score = max(max_score, h_max_scores[i]);
   }
 
-//  cudaMemcpy(&max_score, &d_scores[len - 1], sizeof(int), cudaMemcpyDeviceToHost);
-//  std::cout << cudaGetErrorName(cudaGetLastError()) << std::endl;
   return max_score;
 }
